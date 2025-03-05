7eae90fd87e13801c4f5c7f8996377bd2d52e027
22026522_NguyenThanhDat <126241553+NguyenThanhDat10012004@users.noreply.github.com>
2025-03-05 11:45:20 +0700

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,27 @@
+import torch
+from transformers import BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface.llms import HuggingFacePipeline
+
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.chains import ConversationalRetrievalChain
+
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain import hub
+import os
+from docx import Document
+from tqdm import tqdm
+from datetime import datetime
+from pdf2image import convert_from_path
+from google import genai
+from PIL import Image
+import tempfile
+import pytesseract
+import functools
+from PIL import Image
